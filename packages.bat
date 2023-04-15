33bb146b2a36af5402c05269ee1f0787045571fc
Liqutch <113312256+Liqutch@users.noreply.github.com>
2023-04-15 15:14:43 +0300

    Update packages.bat

--- packages.bat
+++ packages.bat
@@ -1,38 +1,17 @@
 @echo off
-:: Check for Python Installation
-py -3 --version 2>NUL
-if errorlevel 1 goto errorNoPython
+setlocal
 
-:: Reaching here means Python is installed.
-IF EXIST "python-3.9.5-amd64.exe" (
-    del "python-3.9.5-amd64.exe"
+:: Check if Python is installed
+python --version >nul 2>&1
+if %errorlevel% neq 0 (
+  echo Python is not installed. Installing Python...
+  REM Download and install Python from official website
+  powershell -Command "& {Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.9.5/python-3.9.5-amd64.exe' -OutFile 'python-installer.exe'}"
+  python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 )
 
-cls
-
-ECHO Gerekli paketler yukleniyor...
-TIMEOUT 3
-
+echo Installing Python packages...
 py -3 -m pip install -U -r requirements.txt
-
-ECHO Tamamlandi! Simdi main.py'yi calistirabilirsin.
-PAUSE
-
-:: Once done, exit the batch file -- skips executing the errorNoPython section
-goto:eof
-
-:errorNoPython
-echo Error^: Python yuklenmedi veya yola eklenmedi.
-:: set mypath=%cd%
-:: bitsadmin.exe /transfer "InstallPython" https://www.python.org/ftp/python/3.9.5/python-3.9.5-amd64.exe %mypath%\PythonInstaller.exe
-
-IF EXIST "python-3.9.5-amd64.exe" (
-    echo Python Yukleyici hazir, simdi yukle ve/veya Python'u yola ekle.
-) ELSE (
-    echo Installing Python Installer now, this will take a minute or 2.
-    powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.9.5/python-3.9.5-amd64.exe', 'python-3.9.5-amd64.exe')"
-    powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/3.9.5/python-3.9.5-amd64.exe -OutFile python-3.9.5-amd64.exe"   
-    echo Python Yukleyici hazir, simdi yukle ve/veya Python'u yola ekle.
-)
-
-cmd /k
+echo.
+echo Python packages installation complete.
+pause
