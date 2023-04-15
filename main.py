f286b7a54976fce529d14bd36f5b70c2017595ce
Liqutch <113312256+Liqutch@users.noreply.github.com>
2023-04-15 11:18:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from PIL import Image, ImageDraw, ImageFont
 import datetime
+import json
+
+with open("settings.json") as settings:
+  load = json.load(settings)
+  seasonBegin = load['seasonBegin']
+  seasonEnd = load['seasonEnd']
 
 # Season Dates
-beginDate = datetime.datetime.strptime("2023-03-09T13:00:00Z", '%Y-%m-%dT%H:%M:%SZ')
-endDate = datetime.datetime.strptime("2023-06-3T12:00:00Z", '%Y-%m-%dT%H:%M:%SZ')
+beginDate = datetime.datetime.strptime(seasonBegin, '%Y-%m-%dT%H:%M:%SZ')
+endDate = datetime.datetime.strptime(seasonEnd, '%Y-%m-%dT%H:%M:%SZ')
 today = datetime.datetime.today()
 img = Image.open('./assets/background.png')
 total = (endDate - beginDate).days
@@ -20,7 +26,7 @@ bar_y2 = y2 - 1
 draw.rectangle([(bar_x1, bar_y1), (bar_x2, bar_y2)], outline=None, fill=(30, 75, 180))
 percent = ('%{:.2f}'.format((progress / total) * 100))
 print(f"4. Bölüm 2. Sezon {percent} tamamlandı. Bitmesine {total - progress} gün var.")
-img.save("./outputs/progress.png")
+img.save(f"./outputs/{percent}.png")
 
 # Fullscreen
 background = Image.new('RGB', (3840, 2160), (4, 12, 59))
