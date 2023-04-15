5d378738d5ff663492cc6d246bd41af860651ebb
Liqutch <113312256+Liqutch@users.noreply.github.com>
2023-04-15 14:52:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,11 +27,4 @@ draw.rectangle([(bar_x1, bar_y1), (bar_x2, bar_y2)], outline=None, fill=(30, 75,
 percent = ('%{:.2f}'.format((progress / total) * 100))
 print(f"4. Bölüm 2. Sezon {percent} tamamlandı. Bitmesine {total - progress} gün var.")
 img.save(f"./outputs/{percent}.png")
-
-# Fullscreen
-background = Image.new('RGB', (3840, 2160), (4, 12, 59))
-x = int((3840 - 2000) / 2)
-y = int((2160 - 400) / 2)
-background.paste(img, (x, y), img)
-background.save("./outputs/fullscreen.png")
 input("Çıkmak için enter'a basın...\n")
